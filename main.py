c15a79db33ae64870df32639fd7f4da484215828
Eitan03 <eitanabcabc@gmail.com>
2022-11-09 13:38:21 +0000

    changed small param to fix bug

--- main.py
+++ main.py
@@ -20,14 +20,14 @@ async def root():
     return {"message": "Hello from fastAPI1"}
 
 @app.post("/uploadfiles/", status_code=status.HTTP_201_CREATED, response_class=ORJSONResponse)
-async def create_upload_files(fileParts: List[bytes] = File(...), file_name: str = Form(...)):
+async def create_upload_files(fileParts: List[bytes] = File(...), fileName: str = Form(...)):
     try:
-        print(f' recived {file_name} with {len(fileParts)} parts')
+        print(f' recived {fileName} with {len(fileParts)} parts')
 
         if (len(fileParts) != AMOUNT_OF_FILE_PARTS):
             raise Exception("Invalid number of file parts where given!")
 
-        processFile(path.join(IMAGES_PATH, file_name), b''.join(fileParts), LocalJSONLogger(JSON_LOGS_PATH))
+        processFile(path.join(IMAGES_PATH, fileName), b''.join(fileParts), LocalJSONLogger(JSON_LOGS_PATH))
 
     except Exception as e:
 
